6d9452828b62e4a453798cc98cc09534cd83c3d7
chijioke-udokporo <chijithemizzi@yahoo.co.uk>
2025-05-28 15:43:19 +0100

    refactor: update build process and add tsup configuration
    - Changed build script to use tsup for better bundling and minification.
    - Added tsup configuration file for custom build settings including entry point, format, and external dependencies.
    - Updated package.json to include module entry point for ES modules and adjusted exports for compatibility.
    - Added tsup as a development dependency.

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs"],
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  external: ["@octokit/oauth-app", "@octokit/rest", "googleapis", "jsonwebtoken"],
+  treeshake: true,
+  splitting: false,
+  minify: true,
+});
